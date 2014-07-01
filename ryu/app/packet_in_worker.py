e0a12427c3bea2368c7912df7396dda5036458f8
toru.furusawa <toru.furusawa@nttv6.jp>
2014-07-01 10:20:13 +0900

    add packet_in mitigation application that were developed at the SDN/Cloud security hackathon in Okinawa Open Laboratory at June 23-25, 2014.

---
+++ ryu/app/packet_in_worker.py
@@ -0,0 +1,31 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+import time
+
+class PacketInWorker(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(PacketInWorker, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        self.logger.info("----- Connect to OpenFlow Channel dpid:%s -----",
+                         datapath.id)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+        dpid = datapath.id
+        time.sleep(0.1)  # some busy work
+        self.logger.debug("packet in dpid:%s in_port:%s", dpid, in_port)
